991bc6164d3830b5ac926bcc52c7aa6eec522de9
Haibao Tang <tanghaibao@gmail.com>
2010-03-08 22:38:12 -0800

    need a way to remove the trailing ctrl chars when edit files in windows

--- scripts/bed_utils.py
+++ scripts/bed_utils.py
@@ -1,23 +1,23 @@
 class BedLine(object):
     __slots__ = ("seqid", "start", "end", "accn")
 
-    def __init__(self, sline): 
+    def __init__(self, sline):
         args = sline.strip().split("\t")
-        self.seqid = args[0] 
+        self.seqid = args[0]
         self.start = int(args[1])
         self.end = int(args[2])
-        self.accn = args[3] 
+        self.accn = args[3]
 
     def __str__(self):
         return "\t".join(map(str, [getattr(self, attr) \
                 for attr in BedLine.__slots__]))
 
-    def __getitem__(self, key): 
+    def __getitem__(self, key):
         return getattr(self, key)
 
 
 class Bed(list):
-    
+
     def __init__(self, filename):
         self.filename = filename
         beds = []
@@ -27,7 +27,7 @@ class Bed(list):
             beds.append(BedLine(line))
 
         self.seqids = sorted(set(b.seqid for b in beds))
-        self.beds = sorted(beds, key=lambda a: (a.seqid, a.start)) 
+        self.beds = sorted(beds, key=lambda a: (a.seqid, a.start))
 
     def __getitem__(self, i):
         return self.beds[i]
@@ -38,17 +38,17 @@ class Bed(list):
     def __iter__(self):
         for b in self.beds:
             yield b
-	
+
     def get_order(self):
         return dict((f['accn'], (i, f)) for (i, f) in enumerate(self.beds))
-		
+
 
 class RawLine(object):
     __slots__ = ("seqid_a", "pos_a", "seqid_b", "pos_b", "score")
 
-    def __init__(self, sline): 
+    def __init__(self, sline):
         args = sline.strip().split("\t")
-        self.seqid_a = args[0] 
+        self.seqid_a = args[0]
         self.pos_a = int(args[1])
         self.seqid_b = args[2]
         self.pos_b = int(args[3])
@@ -58,13 +58,12 @@ class RawLine(object):
         return "\t".join(map(str, [getattr(self, attr) \
                 for attr in BedLine.__slots__]))
 
-    def __getitem__(self, key): 
+    def __getitem__(self, key):
         return getattr(self, key)
 
-		
 
 class Raw(list):
-	
+
     def __init__(self, filename):
         self.filename = filename
         raws = []
@@ -79,3 +78,4 @@ class Raw(list):
     def __iter__(self):
         for b in self.raws:
             yield b
+
