84ead92eeb0da2e2f53979d56dbe8c1b146cc456
brentp <bpederse@gmail.com>
2010-03-18 14:13:25 -0700

    we want stuff!

--- scripts/bed_utils.py
+++ scripts/bed_utils.py
@@ -3,7 +3,9 @@ Classes to handle the .bed file and .raw file
 """
 
 class BedLine(object):
-    __slots__ = ("seqid", "start", "end", "accn")
+    # the Bed format supports more columns. we only need
+    # the first 4, but keep the information in 'stuff'.
+    __slots__ = ("seqid", "start", "end", "accn", "stuff")
 
     def __init__(self, sline):
         args = sline.strip().split("\t")
@@ -11,10 +13,14 @@ class BedLine(object):
         self.start = int(args[1])
         self.end = int(args[2])
         self.accn = args[3]
+        self.stuff = args[4:] if len(args) > 4 else None
 
     def __str__(self):
-        return "\t".join(map(str, [getattr(self, attr) \
-                for attr in BedLine.__slots__]))
+        s = "\t".join(map(str, [getattr(self, attr) \
+                    for attr in BedLine.__slots__[:-1]]))
+        if self.stuff:
+            s += "\t" + "\t".join(self.stuff)
+        return s
 
     def __getitem__(self, key):
         return getattr(self, key)
@@ -57,7 +63,7 @@ class RawLine(object):
 
     def __str__(self):
         return "\t".join(map(str, [getattr(self, attr) \
-                for attr in BedLine.__slots__]))
+                for attr in RawLine.__slots__]))
 
     def __getitem__(self, key):
         return getattr(self, key)
