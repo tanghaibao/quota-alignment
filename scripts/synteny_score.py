545ba4f787e3c40ea1e91eb2d7bb5f3141f422b7
Haibao Tang <tanghaibao@gmail.com>
2015-03-23 12:27:35 -0700

    implement --lift scripts.synteny_score

--- scripts/synteny_score.py
+++ scripts/synteny_score.py
@@ -37,6 +37,7 @@ import sqlite3
 import itertools
 import os.path as op
 from bisect import bisect_left
+from collections import defaultdict
 
 sys.path.insert(0, op.join(op.dirname(__file__), ".."))
 from grouper import Grouper
@@ -69,12 +70,11 @@ def get_flanker(group, query):
         flanker, other = right_flanker, left_flanker
 
     flanked = not (pos==0 or pos == len(group) or flanker == query)
-    #print >>sys.stderr, flanker, flanked, "\n\n"
 
     return flanker, other, flanked
 
 
-def find_synteny_region(query, sbed, data, window, cutoff, colinear=False):
+def find_synteny_region(query, sbed, data, window, cutoff, colinear=True):
     # get all synteny blocks for a query, algorithm is single linkage
     # anchors are a window centered on query
     # two categories of syntenic regions depending on what query is:
@@ -92,10 +92,12 @@ def find_synteny_region(query, sbed, data, window, cutoff, colinear=False):
         if pos2 - pos1 < window and sbed[pos1].seqid==sbed[pos2].seqid:
             g.join(ia, ib)
 
-    #print list(g)
-
     for group in sorted(g):
         (qflanker, syntelog), (far_flanker, far_syntelog), flanked = get_flanker(group, query)
+
+        # y-boundary of the block
+        gs = [x[1] for x in group]
+        left, right = min(gs), max(gs)
 
         # run a mini-dagchainer here, take the direction that gives us most anchors
         orientation = "+"
@@ -123,28 +125,83 @@ def find_synteny_region(query, sbed, data, window, cutoff, colinear=False):
 
         if score < cutoff: continue
 
-        # y-boundary of the block
-        left, right = group[0][1], group[-1][1]
         # this characterizes a syntenic region (left, right). syntelog is -1 if it's a gray gene
-        syn_region = (syntelog, far_syntelog, left, right, gray, orientation, score)
+        syn_region = (syntelog, left, right, gray, orientation, score)
         regions.append(syn_region)
 
     return sorted(regions, key=lambda x: -x[-1]) # decreasing synteny score
 
 
-def batch_query(qbed, sbed, all_data, options, c=None, transpose=False):
+def validate_syntelog(query, syn_region, qstore, qrank, srank, window, verbose=True):
+    """
+    Validate syntelog calls using the lift BLAST. This improves the accuracy of
+    the original one-pass algorithm. Since localdups were removed during the
+    inital BLAST filtering, hits can get mangled or hidden. `qrank` and `srank`
+    are used to convert ranks between new and raw coordinates.
+    """
+    syntelog, left, right, gray, orientation, score = syn_region
+    ssyntelog, sleft, sright = srank[syntelog], srank[left], srank[right]
+
+    qquery = qrank[query]
+    all_hits = qstore[qquery]
+
+    assert sleft < sright
+    updated = False
+    status = "not updated"
+    for h in all_hits:
+        if sleft - window < h < sright + window:  # Found hit!
+            if gray == 'S':
+                if ssyntelog == h:
+                    status = "syntelog not updated"
+                else:
+                    status = "syntelog updated"
+            else:
+                status = "gray upgraded to syntelog"
+                gray = 'S'
+                score += 1  # Add the penalty back
+            updated = True
+            syntelog = h
+            break
+    if gray == 'S' and not updated:  # Keep syntelog as proxy
+        status = "syntelog downgraded to gray"
+        gray = 'G'
+        score -= 1
+
+    # Only syntelog, gray and score could possibly be changed
+    new_region = syntelog, left, right, gray, orientation, score
+
+    if verbose:
+        print >> sys.stderr, "[STATUS] {0}".format(status)
+        print >> sys.stderr, "Found hits: {0}".format(all_hits)
+        print >> sys.stderr, "Flankers {0} converted to {1}".\
+                                format((left, right), (sleft, sright))
+        print >> sys.stderr, "Before: {0} => {1}".format(query, syn_region)
+        print >> sys.stderr, "After : {0} => {1}".format(query, new_region)
+        print >> sys.stderr
+
+    return new_region, updated
+
+
+def batch_query(qbed, sbed, all_data, options, c=None, transpose=False,
+                lift=None):
 
     cutoff = int(options.cutoff * options.window)
     window = options.window / 2
     sqlite = options.sqlite
     colinear = (options.scoring=="collinear")
     qnote, snote = options.qnote, options.snote
+    if lift:
+        qbedlift, sbedlift, qstore, sstore, qrank, srank = lift
 
     # process all genes present in the bed file
     if transpose:
         all_data = transposed(all_data)
         qbed, sbed = sbed, qbed
         qnote, snote = snote, qnote
+        if lift:
+            qbedlift, sbedlift = sbedlift, qbedlift
+            qstore, sstore = sstore, qstore
+            qrank, srank = srank, qrank
 
     all_data.sort()
     simple_bed = lambda x: (sbed[x].seqid, sbed[x].start)
@@ -158,70 +215,123 @@ def batch_query(qbed, sbed, all_data, options, c=None, transpose=False):
             rmax_pos = bisect_left(all_data, (rmax, 0))
             data = all_data[rmin_pos:rmax_pos]
             regions = find_synteny_region(r, sbed, data, window, cutoff, colinear=colinear)
-            #if not regions: print "%s\t%s" % (qbed[r].accn, "\t".join(["na"]*5))
-            for syntelog, far_syntelog, left, right, gray, orientation, score in regions:
+
+            for syn_region in regions:
                 query = qbed[r].accn
+                lift_update = False
+                # Lift validation
+                if lift:
+                    syn_region, lift_update = validate_syntelog(r, syn_region,
+                                                                qstore, qrank,
+                                                                srank, window)
+
+                syntelog, left, right, gray, orientation, score = syn_region
 
                 left_chr, left_pos = simple_bed(left)
                 right_chr, right_pos = simple_bed(right)
 
-                anchor = sbed[syntelog].accn
+                anchor = sbedlift[syntelog] if lift_update else sbed[syntelog].accn
                 anchor_chr, anchor_pos = simple_bed(syntelog)
                 # below is useful for generating the syntenic region in the coge url
                 left_dist = abs(anchor_pos - left_pos) if anchor_chr==left_chr else 0
                 right_dist = abs(anchor_pos - right_pos) if anchor_chr==right_chr else 0
                 flank_dist = (max(left_dist, right_dist) / 10000 + 1) * 10000
 
-                far_syntelog = sbed[far_syntelog].accn
-
-                left_pos, right_pos = sorted((left_pos, right_pos))
-                data = [query, anchor, gray, score, flank_dist, orientation, far_syntelog]
+                data = [query, anchor, gray, score, flank_dist, orientation, \
+                        qnote, snote]
                 if sqlite:
-                    c.execute("insert into synteny values (?,?,?,?,?,?,?,?)", data[:6]+[qnote,snote])
+                    c.execute("insert into synteny values (?,?,?,?,?,?,?,?)", data)
                 else:
-                    print "\t".join(map(str, data))
+                    pass
+                    #print "\t".join(str(x) for x in data)
 
 
-def main(blast_file, options):
-    qbed_file, sbed_file = options.qbed, options.sbed
-    sqlite = options.sqlite
+def build_order(qbed_file, sbed_file):
+    print >> sys.stderr, "Read annotation files %s and %s" % (qbed_file, sbed_file)
 
-    print >>sys.stderr, "read annotation files %s and %s" % (qbed_file, sbed_file)
+    qbed = Bed(qbed_file)
+    sbed = Bed(sbed_file)
+    qorder = qbed.get_order()
+    sorder = sbed.get_order()
+    return qbed, sbed, qorder, sorder
 
+
+def build_rank_mapping(bed, neworder):
+    """
+    Data structure for linking between rank order after localdups (key) and
+    rank order before localdups (value).
+    """
+    mapping = {}
+    for i, b in enumerate(bed):
+        accn = b.accn
+        if accn not in neworder:
+            print >> sys.stderr, "Lift bedfile miss {0}".format(accn)
+            continue
+        mapping[i] = neworder[accn][0]
+    return mapping
+
+
+def build_store(all_data):
+    """
+    Data structure for fast extraction of lift BLAST (raw BLAST).
+    """
+    print >> sys.stderr, "Build hit store for validation"
+    qstore = defaultdict(list)
+    sstore = defaultdict(list)
+    # Since all_data was already sorted by descending score, the values in the
+    # store are also sorted by descending score (i.e. first one is best hit)
+    for q, s in all_data:
+        qstore[q].append(s)
+        sstore[s].append(q)
+    return qstore, sstore
+
+
+def read_blast(blast_file, qorder, sorder):
+    fp = file(blast_file)
+    print >> sys.stderr, "Read BLAST file %s (total %d lines)" % \
+                        (blast_file, sum(1 for line in fp))
+    fp.seek(0)
+    blasts = sorted([BlastLine(line) for line in fp], \
+                    key=lambda b: -b.score)
+
+    all_data = []
+    for b in blasts:
+        query, subject = b.query, b.subject
+        if query not in qorder or subject not in sorder: continue
+        qi, q = qorder[query]
+        si, s = sorder[subject]
+        all_data.append((qi, si))
+    return all_data
+
+
+def main(blast_file, opts):
+    sqlite = opts.sqlite
+    lift = opts.lift
     try:
-        qbed = Bed(qbed_file)
-        sbed = Bed(sbed_file)
-        qorder = qbed.get_order()
-        sorder = sbed.get_order()
-
-        fp = file(blast_file)
-        print >>sys.stderr, "read BLAST file %s (total %d lines)" % \
-                (blast_file, sum(1 for line in fp))
-        fp.seek(0)
-        blasts = sorted([BlastLine(line) for line in fp], \
-                key=lambda b: b.score, reverse=True)
-
-        all_data = []
-        for b in blasts:
-            query, subject = b.query, b.subject
-            if query not in qorder or subject not in sorder: continue
-            qi, q = qorder[query]
-            si, s = sorder[subject]
-            all_data.append((qi, si))
-
+        qbed, sbed, qorder, sorder = build_order(opts.qbed, opts.sbed)
+        all_data = read_blast(blast_file, qorder, sorder)
+        if lift:
+            qbedlift, sbedlift, qorderlift, sorderlift = build_order(
+                                            opts.qbedlift, opts.sbedlift)
+            raw_data = read_blast(lift, qorderlift, sorderlift)
+            qrank = build_rank_mapping(qbed, qorderlift)
+            srank = build_rank_mapping(sbed, sorderlift)
+            qstore, sstore = build_store(raw_data)
+            lift = (qbedlift, sbedlift, qstore, sstore, qrank, srank)
     except IndexError:
         print >>sys.stderr, "No results were found in the query or source bed file"
+        return -1
 
     c = None
-    if options.sqlite:
-        conn = sqlite3.connect(options.sqlite)
+    if sqlite:
+        conn = sqlite3.connect(sqlite)
         c = conn.cursor()
         c.execute("drop table if exists synteny")
         c.execute("create table synteny (query text, anchor text, gray varchar(1), score integer, dr integer, "
                 "orientation varchar(1), qnote text, snote text)")
 
-    batch_query(qbed, sbed, all_data, options, c=c, transpose=False)
-    batch_query(qbed, sbed, all_data, options, c=c, transpose=True)
+    batch_query(qbed, sbed, all_data, options, c=c, transpose=False, lift=lift)
+    batch_query(qbed, sbed, all_data, options, c=c, transpose=True, lift=lift)
 
     if sqlite:
         c.execute("create index q on synteny (query)")
